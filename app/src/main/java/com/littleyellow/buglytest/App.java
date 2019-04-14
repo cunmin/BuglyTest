409c6734b6b75564d69b0ab85e0f619613cb5a99
littleyellow <450047998@qq.com>
2019-04-14 19:45:28 +0800

    Initial commit

---
+++ app/src/main/java/com/littleyellow/buglytest/App.java
@@ -0,0 +1,28 @@
+package com.littleyellow.buglytest;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId
+        // 调试时，将第三个参数改为true
+        Bugly.init(this, "17675e5a98", true);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        // you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+        // 安装tinker
+        Beta.installTinker();
+    }
+}
---
+++ app/src/main/java/com/littleyellow/buglytest/MainActivity.java
@@ -0,0 +1,31 @@
+package com.littleyellow.buglytest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.tencent.bugly.Bugly;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView test = findViewById(R.id.test);
+        test.setText("1.0.7_不是开发设备");
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ("1.0.7_是开发设备".equals(test.getText().toString())){
+                    test.setText("1.0.7_不是开发设备");
+                    Bugly.setIsDevelopmentDevice(getApplication(), false);
+                }else{
+                    test.setText("1.0.7_是开发设备");
+                    Bugly.setIsDevelopmentDevice(getApplication(), true);
+                }
+            }
+        });
+}
+}
